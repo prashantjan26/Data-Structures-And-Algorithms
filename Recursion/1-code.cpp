cbbaef07bdff576cbaaf87370b6360775582c71f
Prashant Singh <65523870+prashantjan26@users.noreply.github.com>
2022-08-21 10:31:23 +0530

    Setting up Sublime text editor for I/O response

---
+++ Recursion/1-code.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print()
+{
+	cout<< 1 <<endl;
+	print();
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+	#endif
+
+	print();
+
+	return 0;
+}
