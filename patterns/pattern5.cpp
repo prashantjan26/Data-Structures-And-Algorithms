96b3b49072d5ba32bec4826e2fb703b5d59e0b62
Prashant Singh <65523870+prashantjan26@users.noreply.github.com>
2022-08-23 08:06:45 +0530

    Create pattern5.cpp

---
+++ patterns/pattern5.cpp
@@ -0,0 +1,27 @@
+// print the pattern 
+
+// *****
+// ****
+// ***
+// **
+// *
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = n; j > i; j--)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
