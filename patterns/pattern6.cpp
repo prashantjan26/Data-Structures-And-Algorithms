c7edb142c7feb432c2da1d9149a63f7145fe4132
Prashant Singh <65523870+prashantjan26@users.noreply.github.com>
2022-08-23 08:10:13 +0530

    Create pattern6.cpp

---
+++ patterns/pattern6.cpp
@@ -0,0 +1,27 @@
+// print the pattern 
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	for(int i = n; i > 0; i--)
+	{
+		for(int j = 1; j <= i; j++)
+		{
+			cout<<j;
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
