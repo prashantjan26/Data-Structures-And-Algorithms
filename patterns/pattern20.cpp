e617c6f0824fd2f1d0191b457bd2734c33d59445
Prashant Singh <65523870+prashantjan26@users.noreply.github.com>
2022-08-24 21:13:15 +0530

    Create pattern20.cpp

---
+++ patterns/pattern20.cpp
@@ -0,0 +1,52 @@
+// print the pattern
+
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	for(int i = n; i >= 1; i--)
+	{
+		for(int j = n; j >= i; j--)
+		{
+			cout<<"*";
+		}
+		for(int k = i; k > 1; k--)
+			cout<<"  ";
+		for(int k = n; k >= i; k--)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	for(int i = 2; i <= n; i++)
+	{
+		for(int j = n; j >= i; j--)
+		{
+			cout<<"*";
+		}
+		for(int k = 1; k < i; k++)
+			cout<<"  ";
+		for(int k = n; k >= i; k--)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
